d1979133624c1a29d9e8ebb8c7f60bdcf6ba7192
Eric Dobson <eric.n.dobson@gmail.com>
2025-09-21 11:41:21 -0700

    Fix sorting script to be consistent.

--- scripts/everything-imports.sh
+++ scripts/everything-imports.sh
@@ -4,7 +4,7 @@ IMPORTS=`
   find . -name '*.agda' |
   sed -E -e 's|^\./(.*)\.agda|\1|' -e 's|/|.|g' |
   grep -v 'everything' |
-  sort |
+  LC_ALL=C sort |
   sed -E -e 's/(.*)/open import \1 using ()/'`
 
 perl -0777 -i -pe "s/(-- BEGIN IMPORTS\\n).*(\\n-- END IMPORTS)/\$1$IMPORTS\$2/s" everything.agda
